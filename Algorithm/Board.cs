2d279c35329a42b4b53a942848661503ac5f7702
dowankim57 <dowankim57@gmail.com>
2023-01-27 16:22:20 +0900

    레포지토리 생성 ~ Binary Tree 미로 생성 알고리즘

---
+++ Algorithm/Board.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Algorithm
+{
+    class Board
+    {
+        const char CIRCLE = '\u25cf';
+        public ETileType[,] Tile;
+        public int Size;
+
+        public enum ETileType
+        {
+            Empty,
+            Wall
+        }
+
+        public Board(int size)
+        {
+            Debug.Assert(size % 2 == 1); // size는 홀수여야 함
+
+            Tile = new ETileType[size, size];
+            Size = size;
+
+            GenerateByBinaryTree();
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int i = 0; i < Size; ++i)
+            {
+                for (int j = 0; j < Size; ++j)
+                {
+                    Console.ForegroundColor = GetTileColor(Tile[i, j]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor;
+        }
+
+        private void GenerateByBinaryTree()
+        {
+            for (int i = 0; i < Size; ++i)
+            {
+                for (int j = 0; j < Size; ++j)
+                {
+                    if (i % 2 == 0 || j % 2 == 0)
+                    {
+                        Tile[i, j] = ETileType.Wall;
+                    }
+                    else
+                    {
+                        Tile[i, j] = ETileType.Empty;
+                    }
+                }
+            }
+
+            Random rand = new Random();
+
+            for (int i = 0; i < Size; ++i)
+            {
+                for (int j = 0; j < Size; ++j)
+                {
+                    if (i % 2 == 0 || j % 2 == 0)
+                    {
+                        continue;
+                    }
+
+                    if (i == Size - 2 && j == Size - 2)
+                    {
+                        continue;
+                    }
+
+                    if (i == Size - 2)
+                    {
+                        Tile[i, j + 1] = ETileType.Empty;
+                    }
+                    else if (j == Size - 2)
+                    {
+                        Tile[i + 1, j] = ETileType.Empty;
+                    }
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Tile[i, j + 1] = ETileType.Empty;
+                    }
+                    else
+                    {
+                        Tile[i + 1, j] = ETileType.Empty;
+                    }
+                }
+            }
+        }
+
+        private ConsoleColor GetTileColor(ETileType type)
+        {
+            switch (type)
+            {
+                case ETileType.Empty:
+                    return ConsoleColor.Green;
+                case ETileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    Debug.Assert(false);
+                    return ConsoleColor.Black;
+            }
+        }
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board(25);
+            
+            Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                {
+                    continue;
+                }
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+                // 로직
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
